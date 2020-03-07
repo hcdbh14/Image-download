4b59f1b15cb84ec86e61e7d3560882b7aad1b8f1
kenny kurochkin <kennnkuro15@gmail.com>
2020-03-07 12:37:19 +0200

    Initial Commit

---
+++ Image downloading/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Image downloading
+//
+//  Created by Kenny Kurochkin on 07/03/2020.
+//  Copyright © 2020 kenny. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
