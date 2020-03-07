00bdc131c932ed0d007f03a4a6f049bda6c615cd
kenny kurochkin <kennnkuro15@gmail.com>
2020-03-07 15:35:29 +0200

    finish

--- Image downloading/Controller/ImagesViewController.swift
+++ Image downloading/Controller/ImagesViewController.swift
@@ -1,20 +1,23 @@
 import UIKit
 
-private struct Constants {
-    static let numCells = 10
-}
 
-class ImagesViewController: UITableViewController {
+
+class ImagesViewController: UITableViewController, ModelDelegate {
     
+    private let viewModel = MainModel()
+    private var catImages: [UIImage] = []
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        for _ in 0...10 {
+            self.callRandomImageUrl()
+        }
     }
     
     
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return Constants.numCells
+        return catImages.count
     }
     
     
@@ -23,9 +26,31 @@ class ImagesViewController: UITableViewController {
         guard let cell = tableView.dequeueReusableCell(withIdentifier: "CatCell") as? CatCell else {
             return UITableViewCell()
         }
-        cell.callRandomImageUrl()
+        cell.catImage.image = catImages[indexPath.row]
         
         return cell
     }
+    
+    private func callRandomImageUrl() {
+        viewModel.delegate = self
+        DispatchQueue.global().async {
+            self.viewModel.callService()
+        }
+    }
+    
+    internal func didReceiveImageUrl(_ data: String) {
+        guard let url = URL(string: data) else { return }
+        
+        DispatchQueue.global().async {
+            if let data = try? Data(contentsOf: url) {
+                if let image = UIImage(data: data) {
+                    DispatchQueue.main.async {
+                        
+                        self.catImages.append(image)
+                        self.tableView.reloadData()
+                    }
+                }
+            }
+        }
+    }
 }
-
--- Image downloading/Controller/catImageCell.swift
+++ Image downloading/Controller/catImageCell.swift
@@ -1,36 +1,13 @@
 import UIKit
 
-class CatCell: UITableViewCell, ModelDelegate {
-    let viewModel = MainModel()
+class CatCell: UITableViewCell {
     @IBOutlet weak var catImage: UIImageView!
     
-     
-    func callRandomImageUrl() {
-        viewModel.delegate = self
-        DispatchQueue.global().async {
-            self.viewModel.callService()
-        }
-    }
-    
-    func didReceiveImageUrl(_ data: String) {
-        self.catImage.load(url: URL(string: data)!)
-    }
 }
 
 
 
 
 
-extension UIImageView {
-    func load(url: URL) {
-        DispatchQueue.global().async { [weak self] in
-            if let data = try? Data(contentsOf: url) {
-                if let image = UIImage(data: data) {
-                    DispatchQueue.main.async {
-                        self?.image = image
-                    }
-                }
-            }
-        }
-    }
-}
+
+
